faa9a532a6dd71a6ef78542bcaf3ee5159795a3d
Lennard Röttjers <Lennard@rottjers.nl>
2018-05-05 23:16:59 +0200

    added role display and logout

--- includes/views/login_form.php
+++ includes/views/login_form.php
@@ -19,6 +19,29 @@ if (session_status() == PHP_SESSION_NONE) {
         echo("<p>Your discord info: @" . $user->discordName . "#" . $user->discordDiscriminator . "</p>");
         echo("<p>Your discord id: " . $user->id . "</p>");
         echo("<p>Your factorio name: " . $user->factorioIGN . "</p>");
+        echo("<p>Your got the following discord roles: ");
+        $roles = "";
+        foreach($user->roles as $role) {
+            switch($role) {
+                case "231469819147452416":
+                    //hands
+                    $roles .= "Hands, ";
+                    break;
+                case "260086178810822656":
+                    //crew
+                    $roles .= "Crew, ";
+                    break;
+                case "230711907860873216":
+                    //Aliens
+                    $roles .= "Aliens, ";
+                    break;
+                default:
+                    $roles .= "$role, ";
+            }
+        }
+        $roles = rtrim($roles, ", ");
+        echo ("$roles</p>");
+        echo ("<a class='button' href='login/logout.php'>Logout</a>");
     }
     ?>
  </div>
--- login/login.php
+++ login/login.php
@@ -1,5 +1,7 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+php_info();
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config.php';
 require __DIR__ . '/../classes/user.php';
