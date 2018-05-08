0c3a27cd4a610a1ee6d2cac7d1c1102b29a4a895
Lennard Röttjers <Lennard@rottjers.nl>
2018-05-08 23:34:15 +0200

    added rpg

---
+++ classes/rpg.php
@@ -0,0 +1,36 @@
+<?php
+class rpg
+{
+    public $soldier;
+    public $bank;
+    public $builder;
+    public $scientist;
+    public $engineer;
+    public $miner;
+
+    public function __construct($factorioIGN){
+        //query DB
+    }
+    //formula to determine level
+    //(math.ceil( (3.6 + level)^3 / 10) * 100) 
+    public function getSoldier($showExp = true) {
+
+    }
+    public function getBank($showExp = true) {
+        
+    }
+    public function getBuilder($showExp = true) {
+        
+    }
+    public function getScientist($showExp = true) {
+        
+    }
+    public function getEngineer($showExp = true) {
+        
+    }
+    public function getMiner($showExp = true) {
+        
+    }
+
+
+}
